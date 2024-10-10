4a4b9e4af20069a9ca3210ed4cfe51be02bc3a87
unknown <18846090367@163.com>
2024-10-11 05:11:29 +0800

    Q2 SOLVED

--- src/App.jsx
+++ src/App.jsx
@@ -20,16 +20,15 @@ function TravellerRow(props) {
       <td>{traveller.id}</td>
       <td>{traveller.name}</td>
       <td>{traveller.phone}</td>
-      <td>{traveller.bookingTime.toLocaleString()}</td>{/* 转换日期为字符串, React组件中的一个td元素（在TravellerRow组件中）试图渲染一个不是合法React子元素的值。具体来说，错误信息指出一个日期对象（Fri Oct 11 2024 00:17:43 GMT+0800 (中国标准时间)）被直接用作React子元素，这是不允许的。*/}
-      <td>{traveller.destination}</td>
+      <td>{traveller.bookingTime.toLocaleString()}</td> {/* 转换日期为字符串 */}
+      <td>{traveller.destination}</td> 
     </tr>
   );
 }
 
 function Display(props) {
-  
-	/*Q3. Write code to render rows of table, reach corresponding to one traveller. Make use of the TravellerRow function that draws one row.*/
   const { travellers } = props;
+	/*Q3. Write code to render rows of table, reach corresponding to one traveller. Make use of the TravellerRow function that draws one row.*/
   return (
     <table className="bordered-table">
       <thead>
@@ -44,9 +43,7 @@ function Display(props) {
       </thead>
       <tbody>
         {/*Q3. write code to call the JS variable defined at the top of this function to render table rows.*/}
-        {travellers.map(function(traveller) {
-          return <TravellerRow key={traveller.id} traveller={traveller} />;
-        })}
+        {travellers.map(function(traveller) {return <TravellerRow key={traveller.id} traveller={traveller} />;})}
       </tbody>
     </table>
   );
@@ -63,7 +60,8 @@ class Add extends React.Component {
     /*Q4. Fetch the passenger details from the add form and call bookTraveller()*/
     const form = document.forms.addTraveller;
     const newTraveller = {
-      id: this.props.travellers.length + 1,
+      //id: this.props.travellers.length + 1,
+      id: this.props.nextId, // 使用传递的 nextId 作为新乘客的 id
       name: form.travellername.value,
       phone: form.travellerphone.value,
       bookingTime: new Date(),
@@ -121,20 +119,23 @@ class Homepage extends React.Component {
 	return (
 	<div>
 		{/*Q2. Placeholder for Homepage code that shows free seats visually.*/}
+    <h2>Remaining Seats: {10-this.props.bookedSeats}</h2>
 	</div>);
 	}
 }
 class TicketToRide extends React.Component {
   constructor() {
     super();
-    this.state = { travellers: [], selector: 1};
+    this.state = { travellers: [], selector: 1, totalSeats:10, bookedSeats:2, nextId: 3};
     this.bookTraveller = this.bookTraveller.bind(this);
     this.deleteTraveller = this.deleteTraveller.bind(this);
+    this.setSelector = this.setSelector.bind(this);
   }
 
   setSelector(value)
   {
   	/*Q2. Function to set the value of component selector variable based on user's button click.*/
+    this.setState({ selector: value });
   }
   componentDidMount() {
     this.loadData();
@@ -152,7 +153,7 @@ class TicketToRide extends React.Component {
   bookTraveller(passenger) {
 	    /*Q4. Write code to add a passenger to the traveller state variable.*/
       this.setState((prevState) => ({
-        travellers: [...prevState.travellers, passenger],
+        travellers: [...prevState.travellers, passenger], bookedSeats: prevState.bookedSeats + 1, nextId: prevState.nextId + 1
       }));
   }
 
@@ -166,26 +167,41 @@ class TicketToRide extends React.Component {
       {
         newlist.push(element);
       }
-      this.setState({travellers: newlist});
+      this.setState({travellers: newlist, bookedSeats: this.state.bookedSeats-1}); 
     });
   }
   render() {
+    let content;
+    if (this.state.selector === 1) {
+      content = <Homepage travellers={this.state.travellers} bookedSeats={this.state.bookedSeats} />;
+    } else if (this.state.selector === 2) {
+      content = <Display travellers={this.state.travellers} />;
+    } else if (this.state.selector === 3) {
+      content = <Add bookTraveller={this.bookTraveller} travellers={this.state.travellers} nextId={this.state.nextId}/>;
+    } else if (this.state.selector === 4) {
+      content = <Delete deletefunction={this.deleteTraveller} />;
+    }
     return (
       <div>
         <h1>Ticket To Ride</h1>
-	<div>
-	    {/*Q2. Code for Navigation bar. Use basic buttons to create a nav bar. Use states to manage selection.*/}
-	</div>
-	<div>
+	      <nav>
+	      {/*Q2. Code for Navigation bar. Use basic buttons to create a nav bar. Use states to manage selection.*/}
+        <button onClick={() => this.setSelector(1)}>Homepage</button>
+        <button onClick={() => this.setSelector(2)}>Display Travellers</button>
+        <button onClick={() => this.setSelector(3)}>Add Traveller</button>
+        <button onClick={() => this.setSelector(4)}>Delete Traveller</button>
+	      </nav>
+	      <div>
+          {content}
 		{/*Only one of the below four divisions is rendered based on the button clicked by the user.*/}
 		{/*Q2 and Q6. Code to call Instance that draws Homepage. Homepage shows Visual Representation of free seats.*/}
+    {/*<Homepage travellers={this.state.travellers} />*/}
 		{/*Q3. Code to call component that Displays Travellers.*/}
-    <Display travellers={this.state.travellers} />
 		{/*Q4. Code to call the component that adds a traveller.*/}
-    <Add bookTraveller={this.bookTraveller} travellers={this.state.travellers}/>
+    {/*<Add bookTraveller={this.bookTraveller} travellers={this.state.travellers}/>*/}
 		{/*Q5. Code to call the component that deletes a traveller based on a given attribute.*/}
-    <Delete deletefunction={this.deleteTraveller}/>
-	</div>
+    {/*<Delete deletefunction={this.deleteTraveller}/>*/}
+	      </div>
       </div>
     );
   }
